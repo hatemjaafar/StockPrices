b942b855e487add99bd214e74fe7f0e92bfaa788
Hatem Jaafar <jaafar.hatem@gmail.com>
2017-08-13 10:21:27 +0200

    Update routes

--- stock-prices/config/routes.rb
+++ stock-prices/config/routes.rb
@@ -2,4 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 root "application#home"
 
+#route to get last quotes values.
+get '/quotes', to: 'application#actualQuotes'
+
+#route to get quotes values according to the selected period.
+get '/detailQuotes/:name/:beginDate/:endDate', to: 'application#historyQuotes'
 end
