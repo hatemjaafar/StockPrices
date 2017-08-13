8b4deaa8eb807afa3727abd5bc889c38d04f14ba
Hatem Jaafar <jaafar.hatem@gmail.com>
2017-08-14 01:40:55 +0200

    Remove unless code and fix indent.

--- stock-prices/lib/apiRequests.rb
+++ stock-prices/lib/apiRequests.rb
@@ -1,21 +1,14 @@
-require 'uri'
-require 'net/https'
-require 'json'
-
 class ApiRequests
 
-
-		def self.state(code, beginDate, endDate)
-		uri = URI.parse("https://sandbox.tradier.com/v1/markets/history?symbol=#{code}&start=#{beginDate}&end=#{endDate}")
-		#uri = URI.parse("https://sandbox.tradier.com/v1/markets/history?symbol=AAPL&start=2017-01-03&end=2017-01-05")
-		
+	def self.state(code, beginDate, endDate)
+		uri = URI.parse("https://sandbox.tradier.com/v1/markets/history?symbol=#{code}&start=#{beginDate}&end=#{endDate}")		
 		http = Net::HTTP.new(uri.host, uri.port)
 		http.read_timeout = 30
 		http.use_ssl = true
 		http.verify_mode = OpenSSL::SSL::VERIFY_PEER
 		request = Net::HTTP::Get.new(uri.request_uri)
+			
 		# Headers
-
 		request["Accept"] = "application/json"
 		request["Authorization"] = "Bearer 1xlYh47hRZUW7IbB5FvKK9QRT7AL"
 
