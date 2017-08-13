0bd3fb87bd9c3b5f4b8ec516ad8f5f1eff9633eb
Hatem Jaafar <jaafar.hatem@gmail.com>
2017-08-13 10:38:47 +0200

    Update Controller.

--- stock-prices/app/assets/javascripts/main/MainCtrl.js
+++ stock-prices/app/assets/javascripts/main/MainCtrl.js
@@ -2,4 +2,17 @@ angular
  .module('app')
  .controller("MainCtrl", function($scope, getQuotes, detailQuotes){
  	$scope.quotes = getQuotes.get();
+
+ 	$scope.getDetailquotes = function() {
+  		$scope.allQuotes = detailQuotes.get({name:$scope.selectedName, beginDate:$scope.beginDate, endDate:$scope.endDate});      
+  	}
+
+  	$scope.company = [
+  		{ symbol:"AAPL", name: "Apple" },
+   		{ symbol:"FB", name: "Facebook" },
+   		{ symbol:"TSLA", name: "Tesla" },
+   		{ symbol:"AMZN", name: "Amazon" },
+   		{ symbol:"MSFT", name: "Microsoft" },
+   		{ symbol:"NFLX", name: "Netflix" } 
+   	]
  });
