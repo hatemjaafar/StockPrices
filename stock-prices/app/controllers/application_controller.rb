8ae3c5901da497117c60111c09674bb8c37655f6
Hatem Jaafar <jaafar.hatem@gmail.com>
2017-08-13 10:17:11 +0200

    Add the new function to controller

--- stock-prices/app/controllers/application_controller.rb
+++ stock-prices/app/controllers/application_controller.rb
@@ -1,8 +1,24 @@
+require'ApiRequests'
+
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
 def home
 	render 'layouts/application'
+end
+
+def actualQuotes
+	@actualQuotes = ApiRequests.actualQuotes;
+	render json: @actualQuotes 
+end
+
+def historyQuotes
+	puts "je recois mon param"
+	puts params[:name]
+	puts params[:beginDate]
+	puts params[:endDate]
+	@sate = ApiRequests.state(params[:name], params[:beginDate], params[:endDate]);
+	render json: @sate	
 end
 
 end
