706d82db0ff4c93f2e1833048f089ca7b806c79d
Hatem Jaafar <jaafar.hatem@gmail.com>
2017-08-13 15:41:17 +0200

    Add market service.

--- stock-prices/app/assets/javascripts/services/marketServices.js
+++ stock-prices/app/assets/javascripts/services/marketServices.js
@@ -6,5 +6,5 @@ angular
   })
   .factory('detailQuotes', function($resource) {
   	// This factory uses the $resource to make a get request on my Rails route, with the parameters date and city
-   	return $resource('http://localhost:3000/detail/:name/:beginDate/:endDate')
+   	return $resource('http://localhost:3000/detailQuotes/:name/:beginDate/:endDate')
   });
