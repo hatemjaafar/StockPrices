a2cce98ae35d50ce290d5ed3e28724daeef82bbf
Hatem Jaafar <jaafar.hatem@gmail.com>
2017-08-14 01:08:29 +0200

    Fix directive.

--- stock-prices/app/assets/javascripts/directives/myDirective.js
+++ stock-prices/app/assets/javascripts/directives/myDirective.js
@@ -1,64 +1,63 @@
 angular
-  .module('app')
-  .directive('linearChart', function($window){
-   return{
-      restrict:'EA',
-      template:"<svg width='850' height='50'></svg>",
-       link: function(scope, elem, attrs){
-         scope.$watch('quotesData', function(newVal, oldVal){
-        
-        var data = scope.quotesData
-        console.log("je uis dans la directicve")
-var margin = {top: 40, right: 40, bottom: 40, left:40},
-    width = 600,
-    height = 500;
+    .module('app')
+    .directive('linearChart', function($window){
+    return{
+        restrict:'EA',
+        template:"<svg width='850' height='50'></svg>",
+        link: function(scope, elem, attrs){
+            scope.$watch('quotesData', function(newVal, oldVal){
+            d3.select('.chart').remove()
+            var data = scope.quotesData
+            var margin = {top: 40, right: 40, bottom: 40, left:40},
+            width = 600,
+            height = 500;
 
-var x = d3.time.scale()
-    .domain([new Date(data[0].date), d3.time.day.offset(new Date(data[data.length - 1].date), 0)])
-    .rangeRound([0, width - margin.left - margin.right]);
+            var x = d3.time.scale()
+                .domain([new Date(data[0].date), d3.time.day.offset(new Date(data[data.length - 1].date), 0)])
+                .rangeRound([0, width - margin.left - margin.right]);
 
-var y = d3.scale.linear()
-    .domain([0, d3.max(data, function(d) { return d.total; })])
-    .range([height - margin.top - margin.bottom, 0]);
+            var y = d3.scale.linear()
+                .domain([0, d3.max(data, function(d) { return d.total; })])
+                .range([height - margin.top - margin.bottom, 0]);
 
-var xAxis = d3.svg.axis()
-    .scale(x)
-    .orient('bottom')
-    .ticks(d3.time.days, 1)
-    .tickFormat(d3.time.format('%a %d %y'))
-    .tickSize(0)
-    .tickPadding(8);
+            var xAxis = d3.svg.axis()
+                .scale(x)
+                .orient('bottom')
+                .ticks(d3.time.days, 1)
+                .tickFormat(d3.time.format('%a %d %y'))
+                .tickSize(0)
+                .tickPadding(8);
 
-var yAxis = d3.svg.axis()
-    .scale(y)
-    .orient('left')
-    .tickPadding(8);
+            var yAxis = d3.svg.axis()
+                .scale(y)
+                .orient('left')
+                .tickPadding(8);
 
-var svg = d3.select('body').append('svg')
-    .attr('class', 'chart')
-    .attr('width', width)
-    .attr('height', height)
-  .append('g')
-    .attr('transform', 'translate(' + margin.left + ', ' + margin.top + ')');
+            var svg = d3.select('body').append('svg')
+                .attr('class', 'chart')
+                .attr('width', width)
+                .attr('height', height)
+                .append('g')
+                .attr('transform', 'translate(' + margin.left + ', ' + margin.top + ')');
 
-svg.selectAll('.chart')
-    .data(data)
-  .enter().append('rect')
-    .attr('class', 'bar')
-    .attr('x', function(d) { return x(new Date(d.date)); })
-    .attr('y', function(d) { return height - margin.top - margin.bottom - (height - margin.top - margin.bottom - y(d.total)) })
-    .attr('width', 10)
-    .attr('height', function(d) { return height - margin.top - margin.bottom - y(d.total) });
+            svg.selectAll('.chart')
+                .data(data)
+                .enter().append('rect')
+                .attr('class', 'bar')
+                .attr('x', function(d) { return x(new Date(d.date)); })
+                .attr('y', function(d) { return height - margin.top - margin.bottom - (height - margin.top - margin.bottom - y(d.total)) })
+                .attr('width', 10)
+                .attr('height', function(d) { return height - margin.top - margin.bottom - y(d.total) });
 
-svg.append('g')
-    .attr('class', 'x axis')
-    .attr('transform', 'translate(0, ' + (height - margin.top - margin.bottom) + ')')
-    .call(xAxis);
+            svg.append('g')
+                .attr('class', 'x axis')
+                .attr('transform', 'translate(0, ' + (height - margin.top - margin.bottom) + ')')
+                .call(xAxis);
 
-svg.append('g')
-  .attr('class', 'y axis')
-  .call(yAxis);
-        });
-       }
+            svg.append('g')
+                .attr('class', 'y axis')
+                .call(yAxis);
+            });
+        }
    };
 });
