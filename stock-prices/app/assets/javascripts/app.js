3c3053fe0ba5eae08017380456c6c203b67e5574
Hatem Jaafar <jaafar.hatem@gmail.com>
2017-08-13 09:37:29 +0200

    Create the new app.js file

---
+++ stock-prices/app/assets/javascripts/app.js
@@ -0,0 +1,11 @@
+angular
+.module("app", ["ui.router", "templates"])
+.config(["$stateProvider", "$urlRouterProvider",function($stateProvider, $urlRouterProvider) {
+	$stateProvider
+	.state("home", {
+		url: "/home",
+		templateUrl: "main/_index.html",
+		controller: "MainCtrl"
+	})
+	$urlRouterProvider.otherwise("home");
+}])
