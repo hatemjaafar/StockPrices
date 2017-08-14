11f1d1a6acca0f00d37e2f31f1a6f2b12850d19d
Hatem Jaafar <jaafar.hatem@gmail.com>
2017-08-14 04:17:18 +0200

    Fix indent

--- stock-prices/app/assets/javascripts/services/marketServices.js
+++ stock-prices/app/assets/javascripts/services/marketServices.js
@@ -1,10 +1,13 @@
 angular
-  .module('app')
-  .factory('getQuotes', function($resource) {
-  	// This factory uses the $resource to make a get request on my Rails route, with the parameters date and city
-  	return $resource('http://localhost:3000/quotes')
-  })
-  .factory('detailQuotes', function($resource) {
-  	// This factory uses the $resource to make a get request on my Rails route, with the parameters date and city
-   	return $resource('http://localhost:3000/detailQuotes/:name/:beginDate/:endDate')
-  });
+    .module('app')
+    .factory('getQuotes', function($resource) { 
+    
+    // This factory uses the $resource to make a get request on my Rails route, with the parameters date and city
+    return $resource('http://localhost:3000/quotes')
+    })
+
+    .factory('detailQuotes', function($resource) {
+    
+    // This factory uses the $resource to make a get request on my Rails route, with the parameters date and city
+    return $resource('http://localhost:3000/detailQuotes/:name/:beginDate/:endDate')
+    });
